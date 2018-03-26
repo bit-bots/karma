f03e9ded815305bce0fd9a08d8474c69c5b9e40b
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2018-03-26 22:16:29 +0200

    tuning datetimepicker

--- karma/base/static/js/main.js
+++ karma/base/static/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('#datetimepicker').datetimepicker({
-        format: 'Y-m-d H:i' //TODO: can we localize the format?
+        format: 'Y-m-d H:i', //TODO: can we localize the format?
+        inline: true,
+        step: 30,
+        defaultDate:new Date()
     });
-})
+});
