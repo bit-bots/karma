4dc516681b5ad0274192a89c9be88dc69dde8326
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2018-12-03 15:44:37 +0100

    fix login for Django > 2.0.x

--- karma/urls.py
+++ karma/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^karma/', include('karma.karma.urls')),
     url(r'^$', lambda req: redirect('karma_index'), name='index'),
-    url(r'^login$', auth_views.login, name='login', ),
-    url(r'^logout$', auth_views.logout, {
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login', ),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {
         'next_page': '/'
     }, name='logout'),
 ]
