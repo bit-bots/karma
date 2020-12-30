04476ce86aca8f2fad733dd23c5ba2b86df4e159
Timon Engelke <timon.engelke@online.de>
2020-12-30 17:51:16 +0100

    make category selection dependent on project

--- karma/base/static/js/main.js
+++ karma/base/static/js/main.js
@@ -1,3 +1,27 @@
+// https://stackoverflow.com/a/16654226
+$.fn.replaceOptions = function(options) {
+    var self, $option;
+
+    this.empty();
+    self = this;
+
+    $.each(options, function(index, option) {
+        $option = $("<option></option>")
+            .attr("value", option.value)
+            .text(option.text);
+        self.append($option);
+    });
+};
+
+function handleProjectChange() {
+    var project_id = $('#project_selector').val();
+    var project_categories = [{value: "", text: "--------"}];
+    project_categories = project_categories.concat(categories.filter((e) => e.project.toString() === project_id));
+    $('#category_selector').replaceOptions(project_categories);
+}
+
+$('#project_selector').change(handleProjectChange);
+
 $(document).ready(function() {
     $('#datetimepicker').datetimepicker({
         format: 'Y-m-d H:i', //TODO: can we localize the format?
@@ -5,4 +29,5 @@ $(document).ready(function() {
         step: 30,
         defaultDate:new Date()
     });
+    handleProjectChange();
 });
