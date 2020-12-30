f92823e729a8d3cc20f7786c5de30a537a1af4cd
Timon Engelke <timon.engelke@online.de>
2020-12-30 19:19:44 +0100

    fix category selection on edit

--- karma/base/static/js/main.js
+++ karma/base/static/js/main.js
@@ -13,11 +13,18 @@ $.fn.replaceOptions = function(options) {
     });
 };
 
-function handleProjectChange() {
+function handleProjectChange(keepSelection = true) {
     var project_id = $('#project_selector').val();
     var project_categories = [{value: "", text: "--------"}];
     project_categories = project_categories.concat(categories.filter((e) => e.project.toString() === project_id));
+    if (keepSelection) {
+        var previous_category = $('#category_selector').val();
+    }
     $('#category_selector').replaceOptions(project_categories);
+    if (keepSelection) {
+        var selector = '#category_selector option[value="' + previous_category + '"]';
+        $(selector).attr('selected','selected');
+    }
 }
 
 $('#project_selector').change(handleProjectChange);
@@ -29,5 +36,5 @@ $(document).ready(function() {
         step: 30,
         defaultDate:new Date()
     });
-    handleProjectChange();
+    handleProjectChange(true);
 });
