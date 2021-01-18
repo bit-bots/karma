9509de5eae3e9d2c7da9d8528f4c32814b807a15
Timon Engelke <timon.engelke@online.de>
2021-01-18 12:35:21 +0100

    fix copy and paste error

--- karma/api/urls.py
+++ karma/api/urls.py
@@ -9,7 +9,7 @@ app_name = 'api'
 router = DefaultRouter()
 router.register(r'karma', views.KarmaViewSet, basename='karma')
 router.register(r'projects', views.ProjectViewSet, basename='projects')
-router.register(r'highscore', views.HighscoreViewSet, basename='projects')
+router.register(r'highscore', views.HighscoreViewSet, basename='highscore')
 urlpatterns = router.urls
 
 urlpatterns += [
