979544f9c0a81191af708c7e5fe844375d8a4132
Timon Engelke <timon.engelke@online.de>
2024-02-05 17:41:04 +0100

    Change urls to path

--- karma/karma/urls.py
+++ karma/karma/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import url
+from django.urls import path
 from .views import *
 
 urlpatterns = [
-    url(r'^$', index, name='karma_index'),
-    url(r'^personal$', personal_page, name='karma_personal'),
-    url(r'^add_project$', add_project, name='karma_add_project'),
-    url(r'^add_categories$', add_categories, name='karma_add_categories'),
-    url(r'^category/([\d]+)$', category_overview, name='karma_category_overview'),
-    url(r'^project/([\d]+)$', project_overview, name='karma_project_overview'),
-    url(r'^project_highscore/([\d]+)/([\d]+)$', project_highscore, name='karma_project_highscore'),
-    url(r'^project/([\d]+)/([a-zA-Z0-9\.]+)$', project_user, name='karma_project_user'),
-    url(r'^api/project/([\d]+)/active_count/days/([a-zA-Z0-9]+)$', api_project_user_count, name='karma_api_project_user_count'),
-    url(r'^api/project/([\d]+)/active_points/$', api_project_activity_points, name='karma_api_project_activity_count'),
-    url(r'^personal/edit/(\d+)/$', personal_page, name="edit_points"),
-    url(r'^project/rules/$', karma_rules, name="karma_rules"),
+    path('', index, name='karma_index'),
+    path('personal', personal_page, name='karma_personal'),
+    path('add_project', add_project, name='karma_add_project'),
+    path('add_categories', add_categories, name='karma_add_categories'),
+    path('category/<int:category_id>', category_overview, name='karma_category_overview'),
+    path('project/<int:project_id>', project_overview, name='karma_project_overview'),
+    path('project_highscore/<int:project_id>/<int:nr_days>', project_highscore, name='karma_project_highscore'),
+    path('project/<int:project_id>/<str:user_login>', project_user, name='karma_project_user'),
+    path('api/project/<int:project_id>/active_count/days/<int:nr_days>', api_project_user_count, name='karma_api_project_user_count'),
+    path('api/project/<int:project_id>/active_points/', api_project_activity_points, name='karma_api_project_activity_count'),
+    path('personal/edit/<int:point_id>/', personal_page, name="edit_points"),
+    path('project/rules/', karma_rules, name="karma_rules"),
 ]
--- karma/urls.py
+++ karma/urls.py
@@ -1,31 +1,16 @@
-"""karma URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url, include
+"""karma URL Configuration"""
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^karma/', include('karma.karma.urls')),
-    url(r'^$', lambda req: redirect('karma_index'), name='index'),
+    path('admin/', admin.site.urls),
+    path('karma/', include('karma.karma.urls')),
+    path('', lambda req: redirect('karma_index'), name='index'),
     path('api/', include('karma.api.urls')),
     path('calibration/', include('karma.calibration.urls')),
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("auth/openid/", include("simple_openid_connect.integrations.django.urls")),
 ]
